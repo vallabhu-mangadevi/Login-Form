c87d552b9cfe66b8f0c3a67dc6948f151fc16fd2
vallabhu-mangadevi <vallabhumangadevi@gmail.com>
2026-05-07 12:35:11 -0230

    create script.js

--- script.js
+++ script.js
@@ -9,6 +9,12 @@ document.getElementById("password").value;
 let message =
 document.getElementById("message");
 
+let usernamePattern =
+/^[a-zA-Z0-9]+$/;
+
+let passwordPattern =
+/^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[@$!%*?&]).{6,}$/;
+
 if(username === "" || password === ""){
 
 message.innerHTML =
@@ -17,21 +23,28 @@ message.innerHTML =
 message.style.color = "red";
 }
 
-else if(username === "admin"
-&& password === "1234"){
+else if(!usernamePattern.test(username)){
 
 message.innerHTML =
-"Login Successful";
+"Username should contain only letters and numbers";
 
-message.style.color = "green";
+message.style.color = "red";
+}
+
+else if(!passwordPattern.test(password)){
+
+message.innerHTML =
+"Password must contain capital, small, number and symbol";
+
+message.style.color = "red";
 }
 
 else{
 
 message.innerHTML =
-"Invalid Username or Password";
+"Login Successful";
 
-message.style.color = "red";
+message.style.color = "green";
 }
 
 }
