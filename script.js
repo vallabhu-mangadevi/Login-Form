cc8cc84e1a397673f7985b421523da2313160b52
vallabhu-mangadevi <vallabhumangadevi@gmail.com>
2026-05-07 12:46:27 -0230

    create script.js

--- script.js
+++ script.js
@@ -1,50 +1,59 @@
+function togglePassword(){
+  let pass = document.getElementById("password");
+
+  if(pass.type === "password"){
+    pass.type = "text";
+  }else{
+    pass.type = "password";
+  }
+}
+
+// live password hint
+document.getElementById("password").addEventListener("input", function(){
+
+  let value = this.value;
+  let hint = document.getElementById("hint");
+
+  let caps = /[A-Z]/.test(value);
+  let small = /[a-z]/.test(value);
+  let num = /[0-9]/.test(value);
+  let sym = /[@$!%*?&]/.test(value);
+  let len = value.length >= 6;
+
+  hint.innerHTML =
+    `Caps: ${caps ? "✔️" : "✖️"} |
+     Small: ${small ? "✔️" : "✖️"} |
+     Number: ${num ? "✔️" : "✖️"} |
+     Symbol: ${sym ? "✔️" : "✖️"} |
+     Length: ${len ? "✔️" : "✖️"}`;
+});
+
 function login(){
 
-let username =
-document.getElementById("username").value;
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+  let message = document.getElementById("message");
 
-let password =
-document.getElementById("password").value;
+  let usernamePattern = /^[a-zA-Z0-9]+$/;
+  let passwordPattern = /^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[@$!%*?&]).{6,}$/;
 
-let message =
-document.getElementById("message");
+  if(username === "" || password === ""){
+    message.innerHTML = "PLEASE FILL ALL FIELDS";
+    message.style.color = "red";
+  }
 
-let usernamePattern =
-/^[a-zA-Z0-9]+$/;
+  else if(!usernamePattern.test(username)){
+    message.innerHTML = "USERNAME ONLY LETTERS & NUMBERS";
+    message.style.color = "red";
+  }
 
-let passwordPattern =
-/^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[@$!%*?&]).{6,}$/;
+  else if(!passwordPattern.test(password)){
+    message.innerHTML = "WEAK PASSWORD";
+    message.style.color = "red";
+  }
 
-if(username === "" || password === ""){
-
-message.innerHTML =
-"PLEASE FILL ALL FIELDS";
-
-message.style.color = "red";
-}
-
-else if(!usernamePattern.test(username)){
-
-message.innerHTML =
-"USERNAME MUST CONTAIN ONLY LETTERS AND NUMBERS";
-
-message.style.color = "red";
-}
-
-else if(!passwordPattern.test(password)){
-
-message.innerHTML =
-"PASSWORD MUST CONTAIN CAPITAL, SMALL, NUMBER AND SYMBOL";
-
-message.style.color = "red";
-}
-
-else{
-
-message.innerHTML =
-"LOGIN SUCCESSFUL";
-
-message.style.color = "green";
-}
-
+  else{
+    message.innerHTML = "LOGIN SUCCESSFUL";
+    message.style.color = "green";
+  }
 }
